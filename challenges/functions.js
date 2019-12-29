3e5a5b4b1a5ee7ff1ab84d444321102d8fb6ea76
Kolade-Junaid <koladejun@yahoo.com>
2019-12-29 01:24:28 +0000

    Answered all the questions in classes.js, functions.js, objects-arrays.js and prototypes.js files

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,44 +1,41 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
+ * Create a higher-order function named consume with 3 parameters: a, b and cb
+ * The first two parameters can take any argument (we can pass any value as argument)
+ * The last parameter accepts a callback
+ * The consume function should return the invocation of cb, passing a and b into cb as arguments
+ */
 let consume = function(a, b, cb) {
- return cb(a, b)
-}
+  return cb(a, b);
+};
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
- let add = function(a, b) {
-  return a+b;
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+let add = function(a, b) {
+  return a + b;
 };
 
 let multiply = function(a, b) {
-  return a*b;
+  return a * b;
 };
 
-  let greeting = function(firstName, lastName) {
-    return `Hello ${firstName} ${lastName}, nice to meet you`
-  };
-
+let greeting = function(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
- console.log("a",consume(2, 2, add)); // 4
- console.log("b",consume(10, 16, multiply)); // 160
- console.log("c",consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log("a", consume(2, 2, add)); // 4
+console.log("b", consume(10, 16, multiply)); // 160
+console.log("c", consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
-
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
-
+// Explanation:
 
 const external = "I'm outside the function";
 
@@ -48,7 +45,7 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
+  }
   nestedFunction();
 }
 myFunction();
