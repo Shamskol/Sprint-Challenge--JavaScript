656b212417c5184387c71c879ba7b015588f076f
Kolade-Junaid <koladejun@yahoo.com>
2019-12-30 11:26:01 +0000

    Modified the classes.js file to address the error messages

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,5 @@
 // Copy and paste your prototype in here and refactor into a class syntax
-class CuboidMaker {
+class CuboidMakerClass {
   constructor(attributes) {
     this.length = attributes.length;
     this.width = attributes.width;
@@ -16,18 +16,18 @@ class CuboidMaker {
   }
 }
 
-let cuboid = new CuboidMaker({
+let cuboid1 = new CuboidMakerClass({
   length: 4,
   width: 5,
   height: 5
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log("a", cuboid.volume()); // 100
-console.log("b", cuboid.surfaceArea()); // 130
+console.log("a", cuboid1.volume()); // 100
+console.log("b", cuboid1.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
-class CubeMaker extends CuboidMaker {
+class CubeMaker extends CuboidMakerClass {
   constructor(childAttributes) {
     super(childAttributes);
   }
